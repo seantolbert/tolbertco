394c35451cb5a698abedfe794e76511482b67e95
Sean Tolbert <stolbert4150@gmail.com>
2022-05-19 21:54:27 -0400

    stubs up home page

---
+++ src/components/Background.jsx
@@ -0,0 +1,14 @@
+import './Background.css'
+
+export default function Background() {
+  return (
+    <div id="crossfade">
+        <img src="https://i.imgur.com/ERp6JDT.png" alt="background component"/>
+        <img src="https://i.imgur.com/cLOseTM.png" alt="background component" />
+        <img src="https://i.imgur.com/U3Fd1QG.png" alt="background component" />
+        <img src="https://i.imgur.com/972opEe.png" alt="background component" />
+        <img src="https://i.imgur.com/E08ic8g.png" alt="background component" />
+        <img src="https://i.imgur.com/VtTCJVn.png" alt="background component" />
+    </div>
+  )
+}
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+// styles
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <div className="heading-container">
+        <h2 className="home-heading">Tolbert & Co</h2>
+      </div>
+    </div>
+  );
+}
