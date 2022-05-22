9fc2cc5fd0fad343bdd37d0127055d5c9695ca4f
Sean Tolbert <stolbert4150@gmail.com>
2022-05-22 15:07:54 -0400

    fiddling with MenuIcon scss

--- src/components/MenuIcon.jsx
+++ src/components/MenuIcon.jsx
@@ -2,10 +2,21 @@ import "./MenuIcon.scss";
 
 export default function MenuIcon() {
   return (
-    <div className="icon-container">
-      <span className="top bg-primary"></span>
-      <span className="left bg-dark-grey"></span>
-      <span className="right bg-secondary"></span>
+    <div className="menu-container">
+      <span className="glow"></span>
+      {/* <span className="glow slide-left"></span>
+      <span className="glow slide-right"></span> */}
+      <div className="cube">
+        <span className="top">
+          <i className="fa-brands fa-react"></i>
+        </span>
+        <span className="left">
+          <i className="fa-solid fa-address-card"></i>
+        </span>
+        <span className="right">
+          <i className="fa-solid fa-diagram-project"></i>
+        </span>
+      </div>
     </div>
   );
 }
