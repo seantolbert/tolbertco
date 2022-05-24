18441fb5015336aac224b981fe375d5e0d24dbf2
Sean Tolbert <stolbert4150@gmail.com>
2022-05-24 18:54:01 -0400

    figured out graphql page

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -3,7 +3,7 @@ import "./ProjectList.scss";
 import { useGitMetrics } from "../hooks/useGitMetrics";
 
 export default function ProjectList() {
-  const { error, isPending, data: projects } = useGitMetrics()
+  const { error, isPending, data: projects } = useGitMetrics();
   console.log({ projects, error, isPending });
 
   return (
@@ -11,19 +11,19 @@ export default function ProjectList() {
       {error && <div className="error">Something is wrong</div>}
       {isPending && <div>Loading...</div>}
       <div className="text-white">
-        {projects && projects.user.pinnedItems.edges[1].node.name}
+        {/* {projects && projects.user.pinnedItems.edges[1].node.name}
         {projects && projects.user.pinnedItems.edges[1].node.url}
-        {projects && projects.user.pinnedItems.edges[1].node.updatedAt}
+        {projects && projects.user.pinnedItems.edges[1].node.updatedAt} */}
         {projects && projects.user.name}
         {projects && projects.user.url}
-        {projects && projects.user.pinnedItems.totalCount}
-        {/* {projects && pinnedItems.map((item) => {
-          return (
-            <a href={item.url} key={item.id} className="text-white">
-              {item.name}
+        {/* {projects && projects.user.pinnedItems.totalCount} */}
+        {projects &&
+          projects.user.pinnedItems.edges.map(({ node }) => (
+            <a href={node.url} className="flex" key={node.id}>
+              <div>{node.name}</div>
+              <div>Last updated {node.updatedAt}</div>
             </a>
-          )
-        })} */}
+          ))}
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      authorization: `Bearer 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`,
+      authorization: `Bearer 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`,
     },
   };
 });
