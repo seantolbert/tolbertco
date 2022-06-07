68f212aff4ab52b073c35b9092aa35c7ff1dabb7
Sean Tolbert <stolbert4150@gmail.com>
2022-06-07 14:31:02 -0400

    fixed contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,35 +1,34 @@
 import { useState } from "react";
 import axios from "axios";
-// import ReCAPTCHA from "react-google-recaptcha";
 
 export default function Contact() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-  // const [recaptchaToken, setRecaptchaToken] = useState();
+  const [success, setSuccess] = useState(false);
 
   const formId = `${process.env.REACT_APP_FORM_ID}`;
   const formSparkUrl = `https://submit-form.com/${formId}`;
-  // const recaptchaKey = `${process.env.REACT_APP_RECAPTCHA_KEY}`;
-  // const recaptchaRef = useRef();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     await postSubmission();
+    setSuccess(true);
+    setName("");
+    setEmail("");
+    setMessage("");
   };
 
   const postSubmission = async () => {
     const payload = {
-      // "g-recaptcha-response": recaptchaToken,
       name,
       email,
       message,
     };
     try {
       const result = await axios.post(formSparkUrl, payload);
-      console.log(result);
     } catch (err) {
-      console.log(err);
+      return err.message;
     }
   };
 
@@ -42,12 +41,16 @@ export default function Contact() {
       className="flex flex-col items-center w-full bg-dark pb-20"
       onSubmit={handleSubmit}
     >
-      <div className="w-4/5 flex rounded-xl p-5 flex-col group lg:hover:shadow-smallDark items-center transition gap-5">
+      <div className="w-3/5 flex rounded-xl p-5 flex-col group lg:hover:shadow-smallDark items-center transition gap-5">
         <p className="text-4xl text-light">Contact</p>
         <p className="text-xl text-secondary">
           Have a question or want to work together?
         </p>
-
+        {success && (
+          <div className="bg-green-500 rounded-lg border-green text-light p-3">
+            Your message has been sent
+          </div>
+        )}
         <input
           className="caret-secondary text-primary p-2 w-full bg-dark transition duration-600 hover:shadow-pressedMedDark rounded-lg h-10 focus:shadow-medDark"
           required
@@ -71,14 +74,6 @@ export default function Contact() {
           onChange={(e) => setMessage(e.target.value)}
           value={message}
         />
-        {/* <ReCAPTCHA
-        className="p-2 transition duration-1000 group-hover:shadow-medDark rounded-xl"
-          ref={recaptchaRef}
-          sitekey={recaptchaKey}
-          onChange={updateRecaptchaToken}
-          theme="dark"
-          size='compact'
-        /> */}
         <button
           type="submit"
           className="p-3 m-4 rounded-md text-primary text-xl transition duration-1000 transition duration-300 hover:shadow-medDark focus:shadow-medDark active:shadow-pressedMedDark"
